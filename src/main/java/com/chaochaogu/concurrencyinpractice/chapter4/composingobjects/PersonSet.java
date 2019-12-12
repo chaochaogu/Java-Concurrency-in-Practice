c09f594202d4d9137e3e732bfda4e57abcf2297f
chaochao.gu <723454695@qq.com>
2019-12-12 13:36:19 +0800

    add

---
+++ src/main/java/com/chaochaogu/concurrencyinpractice/chapter4/composingobjects/PersonSet.java
@@ -0,0 +1,27 @@
+package com.chaochaogu.concurrencyinpractice.chapter4.composingobjects;
+
+import com.chaochaogu.concurrencyinpractice.model.ThreadSafe;
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
+/**
+ * @author chaochao Gu
+ * @date 2019/12/12
+ */
+@ThreadSafe
+public class PersonSet {
+
+    private final Set<Person> mySet = Sets.newHashSet();
+
+    public synchronized void addPerson(Person p) {
+        mySet.add(p);
+    }
+
+    public synchronized boolean containsPerson(Person p) {
+        return mySet.contains(p);
+    }
+
+    class Person {
+    }
+}
