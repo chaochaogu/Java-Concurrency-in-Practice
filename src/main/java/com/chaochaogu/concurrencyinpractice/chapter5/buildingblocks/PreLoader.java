4ddac3cefaeb66d647b93864660dc43b4bac8f50
chaochao.gu <723454695@qq.com>
2019-12-23 19:17:59 +0800

    add

---
+++ src/main/java/com/chaochaogu/concurrencyinpractice/chapter5/buildingblocks/PreLoader.java
@@ -0,0 +1,45 @@
+package com.chaochaogu.concurrencyinpractice.chapter5.buildingblocks;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+/**
+ * 使用FutureTask来提前加载稍后需要的数据
+ *
+ * @author chaochao Gu
+ * @date 2019/12/23
+ */
+public class PreLoader {
+
+    private final FutureTask<ProductInfo> future = new FutureTask<>(this::loadProductInfo);
+
+    private final Thread thread = new Thread(future);
+
+    public void start() {
+        thread.start();
+    }
+
+    public ProductInfo get() throws DataLoadException, InterruptedException {
+        try {
+            return future.get();
+        } catch (ExecutionException e) {
+            Throwable cause = e.getCause();
+            if (cause instanceof DataLoadException) {
+                throw (DataLoadException) cause;
+            } else {
+                throw LaunderThrowable.launderThrowable(cause);
+            }
+        }
+    }
+
+    private ProductInfo loadProductInfo() throws DataLoadException {
+        return null;
+    }
+
+    interface ProductInfo {
+    }
+
+    private class DataLoadException extends RuntimeException {
+    }
+
+}
